60e6acde50b7dae95c5bd93dcacb32012c66a5c6
David Marsal Ivars <davidmarsalivars@gmail.com>
2023-01-14 15:38:27 -0500

    Add createApp

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,12 @@ import './bootstrap';
 
 import Alpine from 'alpinejs';
 
+import {createApp} from "vue";
+
 window.Alpine = Alpine;
 
 Alpine.start();
+
+createApp({
+    components: {}
+}).mount('app');
