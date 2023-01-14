955f557f3fa5cac9789b5a1377bfba2ee15900a8
David Marsal Ivars <davidmarsalivars@gmail.com>
2023-01-14 13:42:15 -0500

    Update destroy function

--- app/Http/Controllers/Api/CompanyController.php
+++ app/Http/Controllers/Api/CompanyController.php
@@ -64,6 +64,8 @@ class CompanyController extends Controller
      */
     public function destroy(Company $company)
     {
-        //
+        $company->delete();
+
+        return response()->noContent();
     }
 }
