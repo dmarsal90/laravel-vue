61d6efcd9087dd629720bd78eb474cab719e4ef4
David Marsal Ivars <davidmarsalivars@gmail.com>
2023-01-14 23:13:53 -0500

    Update CompanyFactory

--- database/factories/CompanyFactory.php
+++ database/factories/CompanyFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Company>
@@ -17,7 +18,10 @@ class CompanyFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'address' => fake()->address(),
+            'website' => fake()->url()
         ];
     }
 }
